810452bb9b3481004724bb223fda862a0c5d1f46
Harshil <sondagarharshil0.gmail.com>
2023-12-31 22:39:47 +0530

    /tasks route complated

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,11 +1,21 @@
 const config = {
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  server: process.env.SERVER,
-  database: process.env.DATABASE,
+  // user: process.env.USER,
+  // password: process.env.PASSWORD,
+  // server: process.env.SERVER,
+  // database: process.env.DATABASE,
+
+  user: "SharvayaFranchise",
+  password: "sharvaya@2024$",
+  server: "43.231.126.253",
+  database: "SharvayaFranchise",
   options: {
     encrypt: false,
     trustServerCertificate: false
   }
 };
-module.exports = config;
+
+// USER = "SharvayaFranchise";
+// PASSWORD = "sharvaya@2024$";
+// SERVER = "43.231.126.253";
+// DATABASE = "SharvayaFranchise";
+// module.exports = config;
