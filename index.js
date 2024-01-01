32ebe271311c732db35b429ced1c01471dd5786d
Harshil <sondagarharshil0@gmail.com>
2024-01-01 18:48:59 +0530

    &

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-// const express = require("express");
-// // const Routes = require("./src/routes/TodoRoutes");
-// // const config = require("./src/config/dbconfig");
-// // console.log(config);
-// const app = express();
+const express = require("express");
+// const Routes = require("./src/routes/TodoRoutes");
+// const config = require("./src/config/dbconfig");
+// console.log(config);
+const app = express();
+const sql = require("mssql");
+const port = 3000;
+const config = {
+  user: "SharvayaFranchise",
+  password: "sharvaya@2024$",
+  server: "43.231.126.253",
+  database: "SharvayaFranchise",
+  trustServerCertificate: true,
+  options: {
+    encrypt: true,
+    cryptoCredentialsDetails: {
+      minVersion: "TLSv1"
+    }
+  }
+};
+// app.use("/", Routes);
+
+app.get("/", async (req, res) => {
+  const pool = await sql.connect(config);
+  try {
+    const result = await pool
+      .request()
+      .query("SELECT * FROM SharvayaFranchise.dbo.TODO");
+    res.json({ data: result.recordset });
+  } finally {
+    sql.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
+
 // const sql = require("mssql");
-// const port = 3000;
+
 // const config = {
 //   user: "SharvayaFranchise",
 //   password: "sharvaya@2024$",
@@ -13,42 +46,15 @@
 //   authentication: {
 //     type: "default"
 //   },
+//   trustServerCertificate: true,
 //   options: {
-//     encrypt: true
+//     encrypt: true,
+//     cryptoCredentialsDetails: {
+//       minVersion: "TLSv1",
+//       trustServerCertificate: true
+//     }
 //   }
 // };
-// // app.use("/", Routes);
-
-// app.get("/", async (req, res) => {
-//   const pool = await sql.connect(config);
-//   try {
-//     const result = await pool
-//       .request()
-//       .query("SELECT * FROM SharvayaFranchise.dbo.TODO");
-//     res.json({ data: result.recordset });
-//   } finally {
-//     sql.close();
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on ${port}`);
-// });
-
-const sql = require("mssql");
-
-const config = {
-  user: "SharvayaFranchise",
-  password: "sharvaya@2024$",
-  server: "43.231.126.253",
-  database: "SharvayaFranchise",
-  authentication: {
-    type: "default"
-  },
-  options: {
-    encrypt: true
-  }
-};
 
 /*
     //Use Azure VM Managed Identity to connect to the SQL database
@@ -78,32 +84,32 @@ const config = {
     }
 */
 
-console.log("Starting...");
-connectAndQuery();
+// console.log("Starting...");
+// connectAndQuery();
 
-async function connectAndQuery() {
-  try {
-    var poolConnection = await sql.connect(config);
+// async function connectAndQuery() {
+//   try {
+//     var poolConnection = await sql.connect(config);
 
-    console.log("Reading rows from the Table...");
-    var resultSet = await poolConnection
-      .request()
-      .query(`SELECT * FROM SharvayaFranchise.dbo.TODO`);
+//     console.log("Reading rows from the Table...");
+//     var resultSet = await poolConnection
+//       .request()
+//       .query(`SELECT * FROM SharvayaFranchise.dbo.TODO`);
 
-    console.log(`${resultSet.recordset.length} rows returned.`);
+//     console.log(`${resultSet.recordset.length} rows returned.`);
 
-    var columns = "";
-    for (var column in resultSet.recordset.columns) {
-      columns += column + ", ";
-    }
-    console.log("%s\t", columns.substring(0, columns.length - 2));
+//     var columns = "";
+//     for (var column in resultSet.recordset.columns) {
+//       columns += column + ", ";
+//     }
+//     console.log("%s\t", columns.substring(0, columns.length - 2));
 
-    resultSet.recordset.forEach((row) => {
-      console.log("%s\t%s", row.CategoryName, row.ProductName);
-    });
+//     resultSet.recordset.forEach((row) => {
+//       console.log("%s\t%s", row.CategoryName, row.ProductName);
+//     });
 
-    poolConnection.close();
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+//     poolConnection.close();
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
