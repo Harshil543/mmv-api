5ca625c1d94a49c101797e1ab0533a2b6fd4fd95
Harshil <sondagarharshil0.gmail.com>
2024-01-01 14:27:33 +0530

    $

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const express = require("express");
-// const Routes = require("./src/routes/TodoRoutes");
-// const config = require("./src/config/dbconfig");
-// console.log(config);
-const app = express();
+// const express = require("express");
+// // const Routes = require("./src/routes/TodoRoutes");
+// // const config = require("./src/config/dbconfig");
+// // console.log(config);
+// const app = express();
+// const sql = require("mssql");
+// const port = 3000;
+// const config = {
+//   user: "SharvayaFranchise",
+//   password: "sharvaya@2024$",
+//   server: "43.231.126.253",
+//   database: "SharvayaFranchise",
+//   authentication: {
+//     type: "default"
+//   },
+//   options: {
+//     encrypt: true
+//   }
+// };
+// // app.use("/", Routes);
+
+// app.get("/", async (req, res) => {
+//   const pool = await sql.connect(config);
+//   try {
+//     const result = await pool
+//       .request()
+//       .query("SELECT * FROM SharvayaFranchise.dbo.TODO");
+//     res.json({ data: result.recordset });
+//   } finally {
+//     sql.close();
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on ${port}`);
+// });
+
 const sql = require("mssql");
-const port = 3000;
+
 const config = {
   user: "SharvayaFranchise",
   password: "sharvaya@2024$",
@@ -14,23 +46,64 @@ const config = {
     type: "default"
   },
   options: {
-    encrypt: true
+    encrypt: false
   }
 };
-// app.use("/", Routes);
 
-app.get("/", async (req, res) => {
-  const pool = await sql.connect(config);
+/*
+    //Use Azure VM Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        port: process.env["db_port"],
+        database: process.env["db_database"],
+        authentication: {
+            type: 'azure-active-directory-msi-vm'
+        },
+        options: {
+            encrypt: true
+        }
+    }
+
+    //Use Azure App Service Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        port: process.env["db_port"],
+        database: process.env["db_database"],
+        authentication: {
+            type: 'azure-active-directory-msi-app-service'
+        },
+        options: {
+            encrypt: true
+        }
+    }
+*/
+
+console.log("Starting...");
+connectAndQuery();
+
+async function connectAndQuery() {
   try {
-    const result = await pool
-      .request()
-      .query("SELECT * FROM SharvayaFranchise.dbo.TODO");
-    res.json({ data: result.recordset });
-  } finally {
-    sql.close();
-  }
-});
+    var poolConnection = await sql.connect(config);
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
-});
+    console.log("Reading rows from the Table...");
+    var resultSet = await poolConnection
+      .request()
+      .query(`SELECT * FROM SharvayaFranchise.dbo.TODO`);
+
+    console.log(`${resultSet.recordset.length} rows returned.`);
+
+    var columns = "";
+    for (var column in resultSet.recordset.columns) {
+      columns += column + ", ";
+    }
+    console.log("%s\t", columns.substring(0, columns.length - 2));
+
+    resultSet.recordset.forEach((row) => {
+      console.log("%s\t%s", row.CategoryName, row.ProductName);
+    });
+
+    poolConnection.close();
+  } catch (err) {
+    console.error(err.message);
+  }
+}
