4cc9efcb8732e3dd85aafd8ee59b6d6a678dccda
Harshil <sondagarharshil0.gmail.com>
2024-01-02 14:30:48 +0530

    %

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ const app = express();
 const { Sequelize } = require("sequelize");
 const port = 3000;
 const tedious = require("tedious");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+// const sequelize = new Sequelize(
+//   "SharvayaFranchise",
+//   "SharvayaFranchise",
+//   "sharvaya@2024$",
+//   {
+//     host: "43.231.126.253",
+//     dialect: "mssql",
+//     dialectModule: tedious
+//   }
+// );
 
 const sequelize = new Sequelize(
   "SharvayaFranchise",
   "SharvayaFranchise",
-  "sharvaya@2024$",
+  "sharvaya@2024$", // Set username to null
   {
     host: "43.231.126.253",
     dialect: "mssql",
-    dialectModule: tedious
+    dialectModule: tedious,
+    authentication: {
+      type: "azure-active-directory-access-token",
+      options: {
+        tokenFactory: async () => {
+          const credential = new DefaultAzureCredential();
+          const token = await credential.getToken(
+            "https://database.windows.net/"
+          );
+
+          return token?.token || null;
+        }
+      }
+    }
   }
 );
 
