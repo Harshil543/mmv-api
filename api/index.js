d08aa33ef657eef5500d9891032cbda8dc0fa76e
Harshil <sondagarharshil0@gmail.com>
2024-01-02 16:07:49 +0530

    $

--- api/index.js
+++ api/index.js
@@ -39,6 +39,7 @@ app.get("/", async (req, res) => {
     );
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+    console.log(tasks, "tasks");
     res.json({ data: tasks });
   } catch (error) {
     console.error("Error fetching TODOs:", error);
